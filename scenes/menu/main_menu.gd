aef7057e8d8f6da795886191c671d006838d0a33
karasikrus <karasikrus@gmail.com>
2024-06-13 01:58:33 +0300

    test_scene as base scene

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -12,7 +12,7 @@ func _ready():
 func on_play_pressed():
 	SceneTransition.close_screen()
 	await SceneTransition.transition_halfway
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/test_scene/test_scene.tscn")
 	SceneTransition.open_screen()
 
 
